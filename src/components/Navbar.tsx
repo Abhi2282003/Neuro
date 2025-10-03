dddf3a71cf98b0a0694a10c36631247a22643e90
Abhi2282003 <140592089+Abhi2282003@users.noreply.github.com>
2025-10-03 21:51:36 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,30 +30,14 @@ const Navbar = ({ isDisplay }: { isDisplay: boolean }) => {
       <div className="top-0 md:left-0 md:right-0 flex backdrop-blur-sm justify-center py-[10px] border-b border-g items-center font-bold z-50">
         <div className="flex w-full max-w-screen mx-2 md:mx-4 justify-between items-center">
           <div className="flex flex-row gap-2 items-center group">
-            <Link href="/">
+            <Link href="https://neuroguardian.lovable.app/">
               <div className="font-rancho font-bold text-2xl duration-300 pl-2 bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-transparent bg-clip-text ">
                 NeuroGuardian
               </div>
             </Link>
-            <Badge
-              variant={"outline"}
-              className="font-poppins tracking-wider font-thin h-fit rounded "
-            >
-              Alpha
-            </Badge>
+           
           </div>
-          <div className="flex gap-0 md:gap-2 items-center">
-            <ModeToggle disabled={!isDisplay} />
-            <Link
-              href="https://github.com/upsidedownlabs/Chords-Web"
-              target="__blank"
-            >
-              <Button variant={"ghost"} size={"sm"}>
-                <GitHubLogoIcon width={24} height={24} />
-              </Button>
-            </Link>
-            <Contributors />
-          </div>
+          
         </div>
       </div>
     </div>
